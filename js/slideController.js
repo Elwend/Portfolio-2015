0021a5ee7d97630875a9a1f981302c68033ee377
Elwend <elizabeth.wendling@gmail.com>
2016-02-17 19:42:26 -0800

    Portfolio ngshow boolean, media query

--- js/slideController.js
+++ js/slideController.js
@@ -1,9 +1,9 @@
 portfolioApp.controller('slideController', function($scope) {
 
-  $scope.lifespeedSlides = ['../images/lifespeed/1.png', '../images/lifespeed/2.png', '../images/lifespeed/3.png', '../images/lifespeed/4.png', '../images/lifespeed/5.png', '../images/lifespeed/6.png', '../images/lifespeed/7.png', '../images/lifespeed/8.png'];
-
   $scope.pauseOnHover = true;
   $scope.isActive = false;
+
+  $scope.lifespeedSlides = ['../images/lifespeed/1.png', '../images/lifespeed/2.png', '../images/lifespeed/3.png', '../images/lifespeed/4.png', '../images/lifespeed/5.png', '../images/lifespeed/6.png', '../images/lifespeed/7.png', '../images/lifespeed/8.png'];
 
   $scope.activeSlides = function() {
     if ($scope.isActive === true) {
